816e3e7f73d6cf09ca4544f33635031511c0283d
BaLaMyt900 <57708651+BaLaMyt900@users.noreply.github.com>
2023-07-16 14:55:49 +0400

    Добавлены JSON Ответы.

--- DB_functions.py
+++ DB_functions.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import Error
 from config import FSTR_DB_NAME, FSTR_DB_PORT, FSTR_DB_LOGIN, FSTR_DB_PASS, FSTR_DB_HOST
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from Data_class import Data
@@ -55,22 +56,30 @@ class Db:
                             others_titles text,
                             connect text,
                             user_id int,
-                            images int,
+                            image_0 int,
+                            image_1 int,
+                            image_2 int,
                             date_added timestamp DEFAULT CURRENT_TIMESTAMP,
                             coords int NOT NULL,
 							CONSTRAINT user_pk FOREIGN KEY (user_id) REFERENCES Users(id),
                             CONSTRAINT coords_pk FOREIGN KEY (coords) REFERENCES Coords(id) ON DELETE CASCADE,
-                            CONSTRAINT images_pk FOREIGN KEY (images) REFERENCES pereval_images(id) ON DELETE CASCADE
+                            CONSTRAINT image_0_fkey FOREIGN KEY (image_0) REFERENCES pereval_images(id) ON DELETE CASCADE,
+                            CONSTRAINT image_1_fkey FOREIGN KEY (image_1) REFERENCES pereval_images(id) ON DELETE CASCADE,
+                            CONSTRAINT image_2_fkey FOREIGN KEY (image_2) REFERENCES pereval_images(id) ON DELETE CASCADE
                         );''')
         self.cur.close()
         self.conn.close()
 
     def makeconnection(self):
         """ Функция создания соединения с базой данных"""
-        self.conn = psycopg2.connect(dbname=FSTR_DB_NAME, user=FSTR_DB_LOGIN, password=FSTR_DB_PASS, host=FSTR_DB_HOST,
-                                     port=FSTR_DB_PORT)
-        self.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        self.cur = self.conn.cursor()
+        try:
+            self.conn = psycopg2.connect(dbname=FSTR_DB_NAME, user=FSTR_DB_LOGIN, password=FSTR_DB_PASS,
+                                         host=FSTR_DB_HOST,
+                                         port=FSTR_DB_PORT)
+            self.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            self.cur = self.conn.cursor()
+        except (Exception, Error) as error:
+            return {'status': 500, 'message': f'Ошибка подключения к базе данных - {error}', 'id': None}
 
     def stopconnection(self):
         """ Функция разрыва соединения с базой данных """
@@ -80,38 +89,45 @@ class Db:
     def submitdata(self, data: Data):
         """ Принятие новых данных """
         self.makeconnection()
-        # Добавление фотографий TODO Асинхронный ввод фотографий в БД
+        # Добавление фотографий
         images_id = []
         for image in data.images.root:
             self.cur.execute('''INSERT INTO pereval_images (title, img) VALUES (%s, %s) RETURNING id''',
-                             (image.title, image.data, ))
+                             (image.title, image.data,))
             images_id.append(self.cur.fetchone())
 
         # Добавление координат
         self.cur.execute('''INSERT INTO Coords (latitude, longitude, height) VALUES (%s, %s, %s) RETURNING id''',
-                         (data.coords.latitude, data.coords.longitude, data.coords.height, ))
+                         (data.coords.latitude, data.coords.longitude, data.coords.height,))
         coords_id = self.cur.fetchone()
 
-        # # Поиск или добавление пользователя TODO Прокверка ния пользователя иили добавление в БД
-        self.cur.execute(f'''SELECT id FROM Users WHERE email = %s''', (data.user.email, ))
+        # Поиск или добавление пользователя
+        self.cur.execute(f'''SELECT id FROM Users WHERE email = %s''', (data.user.email,))
         user_id = self.cur.fetchone()
 
-        if user_id:
-            self.cur.execute(''' INSERT INTO pereval_added (beautyTitle, title, others_titles, connect, user
-            images, coords) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id''', (data.beauty_title, data.title, data.other_titles,
-                                                                     data.connect, user_id, images_id,
-                                                                     coords_id))
-            return self.cur.fetchone()
-
-        else:
-            self.cur.execute(''' INSERT INTO Users (email, phone, fam, name, oct) VALUES (%s, %s, %s, %s, %s) RETURNING id''',
-                             (data.user.email, data.user.phone, data.user.fam, data.user.name, data.user.oct))
-            user_id = self.cur.fetchone()
-            self.cur.execute(''' INSERT INTO pereval_added (beautyTitle, title, others_titles, connect, user_id,
-                        images, coords) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id''',
+        # Добавление информации в pereval_added
+        if user_id:  # Если пользователь найдет, добавление с имеющимся ИД
+            self.cur.execute(''' INSERT INTO pereval_added (beautyTitle, title, others_titles, connect, user,
+            coords) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id''',
                              (data.beauty_title, data.title, data.other_titles,
-                              data.connect, user_id, images_id, coords_id))
-            return self.cur.fetchone()
+                              data.connect, user_id,
+                              coords_id))
+            object_id = self.cur.fetchone()
+        else:  # Иначе добавление пользователя а потом уже информации
+            self.cur.execute(
+                ''' INSERT INTO Users (email, phone, fam, name, oct) VALUES (%s, %s, %s, %s, %s) RETURNING id''',
+                (data.user.email, data.user.phone, data.user.fam, data.user.name, data.user.oct))
+            user_id = self.cur.fetchone()
+            self.cur.execute(''' INSERT INTO pereval_added (beautyTitle, title, others_titles, connect, user_id, 
+            coords) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id''',
+                             (data.beauty_title, data.title, data.other_titles,
+                              data.connect, user_id, coords_id))
+            object_id = self.cur.fetchone()
 
-        # TODO асинхронная отправка работ с БД, сбор в единый ответ и ответ пользователю PROMISE
-        # ПОИСК ФРЕЙМВОКОВ ДЛЯ РАБОТЫ С БД
+        for id, image_id in enumerate(images_id):
+            self.cur.execute(f''' UPDATE pereval_added SET image_%s = %s WHERE id = %s''', (id, image_id, object_id))
+
+        self.stopconnection()
+
+        return {'status': 200, 'message': None, 'id': object_id}
+
